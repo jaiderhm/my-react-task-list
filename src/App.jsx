39acd6be91f04da1cb340767c294390ac3340068
Jaider Herrera Mora <hjaider003@gmail.com>
2024-01-26 11:21:30 -0500

    sprint review 2 listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import Header from './componentes/header';
-import TaskList from './componentes/TaskList';
-import Task from './componentes/Task';
+// import Header from './componentes/header';
+// import TaskList from './componentes/TaskList';
+// import Task from './componentes/Task';
 
-
+import HookPersonalizado from "./public/Functions";
 
 
 
 function App() {
   return (<>
     <div className='contenedor'>
-      <Header className='Header' />
-      <TaskList className='TaskList' tasks={Task} />
+      <HookPersonalizado/>
     </div>
   </>
   );
